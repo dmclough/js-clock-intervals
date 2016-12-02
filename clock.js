08e52f9a7a6ade208c92142f89e3811bb3708551
dmclough <davidclough@gmail.com>
2016-12-01 21:26:23 -0800

    updated using current time

--- clock.js
+++ clock.js
@@ -1,30 +1,23 @@
 
-var seconds = 0;
+var d = new Date();
+var n = d.getTime();
+//initialize seconds since 1970, adding four hours.
+var seconds = (n /1000) + (60 *60 *4);
+console.log("time since 1970" + n);
 
 var tick = setInterval(clock, 1000);
 
 function clock() {
     seconds++;
-    console.log(seconds);
+console.log(seconds);
     var secondsDegrees = seconds * 6;
-    var minuteDegrees = seconds * 2;
-    var hourDegrees = seconds / 10;
-    console.log("second degrees " + secondsDegrees);
-    console.log("minute degrees " + minuteDegrees);
-    console.log("hour degrees " + hourDegrees);
+    var minuteDegrees = seconds * 6 / 60;
+    var hourDegrees = seconds * 6 / 60 / 12;
+console.log("second degrees " + secondsDegrees);
+console.log("minute degrees " + minuteDegrees);
+console.log("hour degrees " + hourDegrees);
 
     document.getElementById('second').style.transform="rotate("+secondsDegrees+"deg)";
     document.getElementById('minute').style.transform="rotate("+minuteDegrees+"deg)";
     document.getElementById('hour').style.transform="rotate("+hourDegrees+"deg)";
-
-
-    //transform = "rotate(" + secondsDegrees + "deg);";
-
-
-
-
-    //document.getElementById("demo").innerHTML = d.toLocaleTimeString();
 }
-
-
-//spin the dials
